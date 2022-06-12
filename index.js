fa205d3bfbd102bf615c04b90f3d97fa04a3cf33
MirriV <101925718+MirriV@users.noreply.github.com>
2022-06-12 19:18:39 +0200

    Rainbow Challenge

---
+++ index.js
@@ -0,0 +1,35 @@
+var redBox=document.getElementById("red");
+redBox.addEventListener('click', function(){
+    redBox.style.backgroundColor="red";
+
+})
+
+var orangeBox=document.getElementById("orange");
+orangeBox.addEventListener('click', function(){
+    orangeBox.style.backgroundColor="orange";
+})
+
+var yellowBox=document.getElementById("yellow");
+yellowBox.addEventListener('click',function(){
+    yellowBox.style.backgroundColor="yellow";
+})
+
+var greenBox=document.getElementById("green");
+greenBox.addEventListener('click', function(){
+    greenBox.style.backgroundColor="green";
+})
+
+var blueBox=document.getElementById("blue");
+blueBox.addEventListener('click',function(){
+    blueBox.style.backgroundColor="blue";
+})
+
+var purpleBox=document.getElementById("purple");
+purpleBox.addEventListener('click',function(){
+    purpleBox.style.backgroundColor="purple";
+})
+
+var pinkBox=document.getElementById("pink");
+pinkBox.addEventListener('click',function(){
+    pinkBox.style.backgroundColor="pink";
+})
